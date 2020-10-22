6a561cdd8e9e2b3501e52222931f306bcf27babe
kissstrong“git config --global user.email 1612070456@qq.com//git config --global user.name kissstrong“ <1612070456@qq.com>
2020-10-22 10:18:48 +0800

    修改页面

---
+++ AllInOne/src/main/java/com/stads/AllInOneMain80.java
@@ -0,0 +1,15 @@
+package com.stads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author cyz
+ * @date 2020-10-22 9:56
+ */
+@SpringBootApplication
+public class AllInOneMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(AllInOneMain80.class,args);
+    }
+}
